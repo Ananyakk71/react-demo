bfa1c7ff9c1c0fd3846dad838f9f3314ab596e63
ananya <ananyakk71@gmail.com>
2025-02-25 15:50:31 +0530

    practice

---
+++ basicvite/src/App.jsx
@@ -0,0 +1,15 @@
+import youtube from "./youtube"
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite react app</h1>
+      <h1>Vite react app</h1>
+      <youtube />
+    </>
+  )
+}
+
+export default App                
+    
